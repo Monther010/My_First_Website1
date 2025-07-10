9c95f9a0b6f7eac08e0f62535cc7ed632da72cbd
Monther010 <164104655+Monther010@users.noreply.github.com>
2025-07-10 11:59:53 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-  document.addEventListener("DOMContentLoaded", function() {
-    const button = document.getElementById("changeColorBtn");
-    button.addEventListener("click", function() {
-      const colors = ["#89CFF0", "#ff5733", "#92574b", "#d2b48c", "#c48e76"];
-      const randomcolor = colors[Math.floor(Math.random()* colors.length)];
-      document.body.style.background = randomcolor;
-    });
-  });
